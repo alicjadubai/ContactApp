c1e4749b64bcfb4c470779a37e38f440730d6869
alicjadubai <alicja_dubai@wp.pl>
2023-04-01 14:26:46 +0200

    Chyba final

--- src/ContactApp/List.js
+++ src/ContactApp/List.js
@@ -9,16 +9,54 @@ const List = ({
   personnel,
   filter,
   activeLetter,
+  onClick,
   selectedDivision,
   selectedBranch,
   CurrentPagination,
   setCurrentPagination,
   filteredNat,
+  itemsPerPage,
+  setItemsPerPage,
 }) => {
-  const itemsPerPage = 7;
+  // Apply filters to the items array
+  const filteredData = items
+    .map((item) => {
+      const personnelItem = personnel?.find((p) => p.Id === item.PersonnelId);
+      const nationalityItem = nationality.find(
+        (p) => p.Id === personnelItem?.nationalityId
+      );
+      const pictureItem = pictures.find(
+        (p) => p.PersonnelId === personnelItem?.Id
+      );
+
+      return {
+        item,
+        pictureItem,
+        nationalityItem,
+        personnelItem,
+      };
+    })
+    .filter(
+      (el) =>
+        (!activeLetter || el.personnelItem?.FL === activeLetter) &&
+        (!filter ||
+          el.personnelItem?.lastName.toUpperCase().includes(filter)) &&
+        (!selectedDivision ||
+          selectedDivision === "Select division" ||
+          el.item.DivisionId == selectedDivision) &&
+        (!selectedBranch ||
+          selectedBranch === "Select division" ||
+          el.item.BranchId == selectedBranch) &&
+        (!filteredNat ||
+          filteredNat === "Select nationality" ||
+          el.personnelItem?.nationalityId == filteredNat)
+    );
+
   const lastIndex = CurrentPagination * itemsPerPage;
   const firstIndex = lastIndex - itemsPerPage;
-  const currentData = items.slice(firstIndex, lastIndex);
+  const currentData = filteredData.slice(firstIndex, lastIndex);
+
+  setItemsPerPage(8);
   return (
     <>
       <div id="list">
@@ -37,71 +75,39 @@ const List = ({
               </tr>
             </thead>
             <tbody>
-              {currentData
-                .map((item) => {
-                  const personnelItem = personnel?.find(
-                    (p) => p.Id === item.PersonnelId
-                  );
-                  const nationalityItem = nationality.find(
-                    (p) => p.Id === personnelItem?.nationalityId
-                  );
-
-                  const pictureItem = pictures.find(
-                    (p) => p.PersonnelId === personnelItem?.Id
-                  );
-                  return {
-                    item,
-                    pictureItem,
-                    nationalityItem,
-                    personnelItem,
-                  };
-                })
-                .filter(
-                  (el) =>
-                    (!activeLetter || el.personnelItem?.FL === activeLetter) &&
-                    (!filter ||
-                      el.personnelItem?.lastName
-                        .toUpperCase()
-                        .includes(filter)) &&
-                    (!selectedDivision ||
-                      el.item.DivisionId == selectedDivision) &&
-                    (!selectedBranch || el.item.BranchId == selectedBranch) &&
-                    (!filteredNat ||
-                      el.personnelItem.nationalityId == filteredNat)
-                )
-                .map((el, index) => (
-                  <tr key={el.item.Id}>
-                    {el.pictureItem ? (
-                      <td>
-                        <img
-                          className="pic"
-                          src={`https://hqmncnetest.sharepoint.com/sites/telephoneDirectory2/Pictures/${el.pictureItem.Title}.jpg`}
-                        />
-                      </td>
-                    ) : (
-                      <td></td>
-                    )}
-                    <td>{el.personnelItem?.Title || ""}</td>
+              {currentData.map((el, index) => (
+                <tr key={el.item.Id}>
+                  {el.pictureItem ? (
                     <td>
-                      {el.nationalityItem ? (
-                        <span className="details">
-                          {el.nationalityItem.Title}
-                        </span>
-                      ) : null}
+                      <img
+                        className="pic"
+                        src={`https://hqmncnetest.sharepoint.com/sites/telephoneDirectory2/Pictures/${el.pictureItem.Title}.jpg`}
+                      />
                     </td>
-                    <td>{el.personnelItem?.firstName || ""}</td>
-                    <td>{el.personnelItem?.lastName || ""}</td>
-                    {el.personnelItem?.phone ? (
-                      <td>{el.personnelItem.phone}</td>
+                  ) : (
+                    <td></td>
+                  )}
+                  <td>{el.personnelItem?.Title || ""}</td>
+                  <td>
+                    {el.nationalityItem ? (
+                      <span className="details">
+                        {el.nationalityItem.Title}
+                      </span>
                     ) : null}
-                    {el.personnelItem?.email ? (
-                      <td>{el.personnelItem.email}</td>
-                    ) : null}
-                    <td>
-                      <Structure item={el.item} />
-                    </td>
-                  </tr>
-                ))}
+                  </td>
+                  <td>{el.personnelItem?.firstName || ""}</td>
+                  <td>{el.personnelItem?.lastName || ""}</td>
+                  {el.personnelItem?.phone ? (
+                    <td>{el.personnelItem.phone}</td>
+                  ) : null}
+                  {el.personnelItem?.email ? (
+                    <td>{el.personnelItem.email}</td>
+                  ) : null}
+                  <td>
+                    <Structure item={el.item} />
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
@@ -113,7 +119,7 @@ const List = ({
           setCurrentPagination={setCurrentPagination}
           itemsPerPage={itemsPerPage}
           CurrentPagination={CurrentPagination}
-          items={items}
+          items={filteredData}
         />
       </div>
     </>
--- src/ContactApp/Titles.js
+++ src/ContactApp/Titles.js
@@ -16,103 +16,105 @@ const Tiles = ({
   setCurrentPagination,
   filteredNat,
   itemsPerPage,
+  setItemsPerPage,
 }) => {
+  // Apply filters to the items array
+  const filteredData = items
+    .map((item) => {
+      const personnelItem = personnel?.find((p) => p.Id === item.PersonnelId);
+      const nationalityItem = nationality.find(
+        (p) => p.Id === personnelItem?.nationalityId
+      );
+      const pictureItem = pictures.find(
+        (p) => p.PersonnelId === personnelItem?.Id
+      );
+
+      return {
+        item,
+        pictureItem,
+        nationalityItem,
+        personnelItem,
+      };
+    })
+    .filter(
+      (el) =>
+        (!activeLetter || el.personnelItem?.FL === activeLetter) &&
+        (!filter ||
+          el.personnelItem?.lastName.toUpperCase().includes(filter)) &&
+        (!selectedDivision ||
+          selectedDivision === "Select division" ||
+          el.item.DivisionId == selectedDivision) &&
+        (!selectedBranch ||
+          selectedBranch === "Select division" ||
+          el.item.BranchId == selectedBranch) &&
+        (!filteredNat ||
+          filteredNat === "Select nationality" ||
+          el.personnelItem?.nationalityId == filteredNat)
+    );
+
   const lastIndex = CurrentPagination * itemsPerPage;
   const firstIndex = lastIndex - itemsPerPage;
-  const currentData = items.slice(firstIndex, lastIndex);
+  const currentData = filteredData.slice(firstIndex, lastIndex);
+
+  setItemsPerPage(8);
   return (
     <>
       <div id="tiles">
-        {currentData
-          .map((item) => {
-            const personnelItem = personnel?.find(
-              (p) => p.Id === item.PersonnelId
-            );
-            const nationalityItem = nationality.find(
-              (p) => p.Id === personnelItem?.nationalityId
-            );
+        {currentData.map((el, index) => (
+          // picture
+          <div key={el.item.Id} className="thumbnail">
+            {el.pictureItem ? (
+              <div onClick={() => onClick(el.item.Id)} className="pic">
+                <img
+                  src={`https://hqmncnetest.sharepoint.com/sites/telephoneDirectory2/Pictures/${el.pictureItem.Title}.jpg`}
+                />
+              </div>
+            ) : (
+              <div className="pic">
+                <img src={`https://image.ibb.co/gE7cc5/user_4.jpg`} />
+              </div>
+            )}
 
-            const pictureItem = pictures.find(
-              (p) => p.PersonnelId === personnelItem?.Id
-            );
+            <div className="details ">
+              <p>
+                {el.personnelItem ? (
+                  <span>
+                    {el.personnelItem.Title} {el.personnelItem.firstName}{" "}
+                    {el.personnelItem.lastName}
+                  </span>
+                ) : null}
+              </p>
 
-            return {
-              item,
-              pictureItem,
-              nationalityItem,
-              personnelItem,
-            };
-          })
-          .filter(
-            (el) =>
-              (!activeLetter || el.personnelItem?.FL === activeLetter) &&
-              (!filter ||
-                el.personnelItem?.lastName.toUpperCase().includes(filter)) &&
-              (!selectedDivision ||
-                selectedDivision === "Select division" ||
-                el.item.DivisionId == selectedDivision) &&
-              (!selectedBranch ||
-                selectedBranch === "Select division" ||
-                el.item.BranchId == selectedBranch) &&
-              (!filteredNat ||
-                filteredNat === "Select nationality" ||
-                el.personnelItem?.nationalityId == filteredNat)
-          )
-          .map((el, index) => (
-            // picture
-            <div key={el.item.Id} className="thumbnail">
-              {el.pictureItem ? (
-                <div onClick={() => onClick(el.item.Id)} className="pic">
-                  <img
-                    src={`https://hqmncnetest.sharepoint.com/sites/telephoneDirectory2/Pictures/${el.pictureItem.Title}.jpg`}
-                  />
-                </div>
-              ) : (
-                <div className="pic">
-                  <img src={`https://image.ibb.co/gE7cc5/user_4.jpg`} />
-                </div>
-              )}
-
-              <div className="details ">
-                <p>
-                  {el.personnelItem ? (
-                    <span>
-                      {el.personnelItem.Title} {el.personnelItem.firstName}{" "}
-                      {el.personnelItem.lastName}
-                    </span>
-                  ) : null}
-                </p>
-
-                <p className="details__nat">
-                  {el.nationalityItem ? (
-                    <>
-                      <img className="flag" src={el.nationalityItem.flag.Url} />
-                      <span>{el.nationalityItem.Title}</span>
-                    </>
-                  ) : null}
-                </p>
-                <div
-                  className={
-                    el.item.isDetailsHidden ? "details" : "details__hidden"
-                  }
-                >
-                  {el.personnelItem?.phone ? (
-                    <div className="details_phone">
-                      <span className="material-symbols-outlined">call</span>{" "}
-                      <span>{el.personnelItem.phone}</span>
-                    </div>
-                  ) : null}
-                  {el.personnelItem?.email ? (
-                    <div className="details_email">
-                      <span className="material-symbols-outlined">mail</span>
-                      <span>{el.personnelItem.email}</span>
-                    </div>
-                  ) : null}
-                  <Structure item={el.item} />
-                </div>
+              <p className="details__nat">
+                {el.nationalityItem ? (
+                  <>
+                    <img className="flag" src={el.nationalityItem.flag.Url} />
+                    <span>{el.nationalityItem.Title}</span>
+                  </>
+                ) : null}
+              </p>
+              <div
+                className={
+                  el.item.isDetailsHidden ? "details" : "details__hidden"
+                }
+              >
+                {el.personnelItem?.phone ? (
+                  <div className="details_phone">
+                    <span className="material-symbols-outlined">call</span>{" "}
+                    <span>{el.personnelItem.phone}</span>
+                  </div>
+                ) : null}
+                {el.personnelItem?.email ? (
+                  <div className="details_email">
+                    <span className="material-symbols-outlined">mail</span>
+                    <span>{el.personnelItem.email}</span>
+                  </div>
+                ) : null}
+                <Structure item={el.item} />
               </div>
             </div>
-          ))}
+          </div>
+        ))}
       </div>
       <div>
         <Pagination
@@ -121,7 +123,7 @@ const Tiles = ({
           setCurrentPagination={setCurrentPagination}
           itemsPerPage={itemsPerPage}
           CurrentPagination={CurrentPagination}
-          items={items}
+          items={filteredData}
         />
       </div>
     </>
